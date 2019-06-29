5c3e35d9c04689ae11e01d4d1cd27bfe42d90eb9
aprilmariewilson <aprilmariewilson@yahoo.com>
2019-06-29 17:37:05 -0600

    first commit

---
+++ build/MatchResult.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//enums are small fixed set of values that are closely related
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
---
+++ src/MatchResult.ts
@@ -0,0 +1,6 @@
+//enums are small fixed set of values that are closely related
+export enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
